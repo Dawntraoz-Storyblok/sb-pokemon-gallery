140542b8bb2ccae6ccc7bca6ce683da8d73549e4
Dawntraoz <albasilventefu@gmail.com>
2022-09-12 15:54:31 +0200

    feat: auto fit grid

--- storyblok/Album.vue
+++ storyblok/Album.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-editable="blok"
-    class="container grid grid-flow-col auto-cols-max gap-8 py-12"
+    class="container grid grid-cols-[repeat(auto-fit,332px)] justify-center gap-10 py-12"
   >
     <StoryblokComponent
       v-for="blok in blok.cards"
