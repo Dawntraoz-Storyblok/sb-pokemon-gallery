430334c7c39ab5189b1c2d4731691814438a7477
Dawntraoz <albasilventefu@gmail.com>
2022-09-13 11:19:26 +0200

    feat: optimized image component

--- storyblok/Card.vue
+++ storyblok/Card.vue
@@ -1,6 +1,18 @@
 <template>
-  <article class="bg-[#271B46] rounded-xl p-4">
-    <h3>{{ blok.title }}</h3>
+  <article v-editable="blok" class="bg-[#271B46] rounded-xl p-4 pb-6">
+    <StoryblokComponent
+      v-for="blok in blok.image"
+      :key="blok._uid"
+      :blok="blok"
+    />
+    <header class="pt-4 flex gap-4 items-center">
+      <div class="rounded-full w-8 h-8" :style="`background-color: ${blok.color.color}`"></div>
+      <h3 class="flex flex-col">
+        {{ blok.title }}
+        <span class="font-sans font-thin text-xs">{{ blok.subtitle}}</span>
+      </h3>
+      <button class="ml-auto bg-purple-900 rounded-full px-4 py-1">{{ blok.button_text }}</button>
+    </header>
   </article>
 </template>
 
---
+++ storyblok/Image.vue
@@ -0,0 +1,66 @@
+<template>
+  <picture v-editable="blok">
+    <template v-if="art_direction">
+      <source
+        v-for="{ image, media_condition, width, height } in art_direction"
+        :media="media_condition"
+        :srcset="createImage(image.filename, width, height, image.focus)"
+        :width="width"
+        :height="height"
+      >
+    </template>
+    <img
+      :src="createImage(filename, width, height)"
+      :width="width"
+      :height="height"
+      :srcset="srcset"
+      :sizes="responsive_conditions"
+      :alt="alt"
+      :loading="loading"
+      class="shadow-lg w-full"
+      :class="{ 'rounded-xl': rounded }"
+    />
+  </picture>
+</template>
+
+<script setup>
+const props = defineProps({ blok: Object })
+
+const { width, height, rounded, loading } = props.blok
+const { art_direction } = props.blok
+
+const { filename, alt, focus } = props.blok.original_image
+
+let srcset = ref('')
+
+const createImage = (original, width, height, focal = focus) => {
+  return `${original}/m/${width}x${height}/filters:focal(${focal})`
+};
+
+// Responsive images by density
+const { density_2x, density_3x } = props.blok
+
+if (density_2x || density_3x) {
+  let densitiesSrcset = `${createImage(filename, width, height)} 1x`
+  densitiesSrcset += density_2x ? `, ${createImage(filename, width * 2, height * 2)} 2x` : ''
+  densitiesSrcset += density_3x ? `, ${createImage(filename, width * 3, height * 3)} 3x` : ''
+
+  srcset.value = densitiesSrcset
+}
+
+// Responsive images by width
+const { responsive_widths, responsive_conditions } = props.blok
+
+if (responsive_widths) {
+  const aspectRatio = width / height
+  const responsiveImages = responsive_widths.split(',')
+
+  let widthsSrcset = ''
+  responsiveImages.map(imageWidth => {
+    widthsSrcset += `${createImage(filename, imageWidth, Math.round(imageWidth / aspectRatio))} ${imageWidth}w,`
+    return true
+  })
+
+  srcset.value = widthsSrcset
+}
+</script>
